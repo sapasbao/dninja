47119a958ab1578d38d64d5a214b8d99c7e86a54
sapasbao <sapasflash@gmail.com>
2025-02-05 12:22:12 +0000

    correctly installed tasks_home, in INSTALLED_APPS

--- dTasks/tasks/tasks/settings.py
+++ dTasks/tasks/tasks/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    ""
+    "tasks_home",
 ]
 
 MIDDLEWARE = [
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "tasks_home",
 ]
 
 ROOT_URLCONF = "tasks.urls"
